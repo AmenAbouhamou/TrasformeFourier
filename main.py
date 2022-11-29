f491bf541db93db41f2ca4a4424599586d6cd568
AmenAbouhamou <amen.abouhamou18@gmail.com>
2022-11-29 09:21:40 +0100

    test Fourier Rapide

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from numpy.random import default_rng as rng
 from FourierDiscreta1D import *
 from FourierDiscreta2D import *
 from FourierRapide1D import *
+from FourierRapide2D import *
 import numpy as np
 import cmath as math
 from operators import *
@@ -24,12 +25,14 @@ img1DTI = np.zeros(N, dtype=complex)
 img1DTDB = np.zeros(N, dtype=complex)
 img2DTI = np.zeros((M,N), dtype=complex)
 img2DTDB = np.zeros((M,N), dtype=complex)
+print("Dimension=> ",M," - ",N)
+p1D = np.random.rand(N)
 
-mode=2
+mode=4
 match mode:
     case 1:
         # 1D
-        p1D = np.random.rand(n)
+       
         start_time = time.time()
         img1DTDB = FourierBrut1D(p1D)
         print("--- %s seconds Fourier Brut 1D ---" % (time.time() - start_time))
@@ -55,14 +58,17 @@ match mode:
 
         print("Inverse :")
         comparaison(img1DTI, temoin1D_inv)
-        
+        print2Dresult(img1DTDB)
+        print2Dresult(img1DTI)
     case 2:
+        
         start_time = time.time()
         img2DTDB = FourierBrut2D(p)
         print("--- %s seconds Fourier Brut 2D ---" % (time.time() - start_time))
         start_time = time.time()
         img2DTI = FourierBrutInverse2D(img2DTDB)
         print("--- %s seconds Fourier Brut Inv 2D ---" % (time.time() - start_time))
+       
         start_time = time.time()
         temoin = np.fft.fft2(p)
         print("--- %s seconds Fourier Rapide PY 2D ---" % (time.time() - start_time))
@@ -76,8 +82,64 @@ match mode:
         img2DTI = traitement(img2DTI)
 
         print("Transformée directe : ")
-        comparaison(img2DTDB, temoin) 
-        print(img2DTI[0][0], temoin_inv[0][0])
+        compaire(img2DTDB, temoin)
+        
         print("Transformée inverse :")
-        comparaison(img2DTI, temoin_inv)
+        compaire(img2DTI, temoin_inv)
+        print2Dresult(img2DTDB)
+        print2Dresult(img2DTI)
+        
+    case 3:
+        start_time = time.time()
+        img1DTDB = FourierRapide1D(p1D)
+        print("--- %s seconds Fourier Rapide 1D ---" % (time.time() - start_time))
+        
+        start_time = time.time()
+        img1DTDI = FourierRapideInverse1D(img1DTDB)
+        print("--- %s seconds Fourier Rapide INV 1D ---" % (time.time() - start_time))
+        start_time = time.time()
+        temoin1D = np.fft.fft(p1D)
+        print("--- %s seconds Fourier Rapide PY 1D ---" % (time.time() - start_time))
+        start_time = time.time()
+        temoin1D_inv = np.fft.ifft(temoin1D)
+        print("--- %s seconds Fourier Rapide INV PY 1D ---" % (time.time() - start_time))
+
+        temoin1D = traitement(temoin1D)
+        temoin1D_inv = traitement(temoin1D_inv)
+        img1DTDB = traitement(img1DTDB)
+        img1DTDI = traitement(img1DTDI)
+
+        print("Directe :")
+        comparaison(img1DTDB, temoin1D)
+        print("Inverse :")
+        comparaison(img1DTDI, temoin1D_inv)
+    case 4:
+         # 2D RAPIDE
+
+        img2DTDB = FFT2D(p)
+        img2DTI = IFFT2D(img2DTDB)
+        
+        start_time = time.time()
+        temoin = np.fft.fft2(p)
+        print("--- %s seconds Fourier Rapide PY 2D ---" % (time.time() - start_time))
+        start_time = time.time()
+        temoin_inv = np.fft.ifft2(temoin)
+        print("--- %s seconds Fourier Rapide INV PY 2D ---" % (time.time() - start_time))
+        
+        temoin = traitement(temoin)
+        temoin_inv = traitement(temoin_inv)
+        img2DTDB = traitement(img2DTDB)
+        img2DTI = traitement(img2DTI)
+
+        img2DTDB = pow_to_or(img2DTDB, M_org, N_org)
+        temoin = pow_to_or(temoin, M_org, N_org)
+        img2DTI = pow_to_or(img2DTI, M_org, N_org)
+        temoin_inv = pow_to_or(temoin_inv, M_org, N_org)
+
+        print("Transformée directe rapide : ")
+        comparaison(img2DTDB, temoin)
+
+        print("Transformée inverse rapide : ")
+        comparaison(img2DTI, temoin_inv)   
+        
         
